526653f5f614e813def05c670a4edc631daadde7
zem <143418708+zemsintchie@users.noreply.github.com>
2024-01-13 15:44:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: ".header",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x43ff3f,
+  backgroundColor: 0xffffff,
+  points: 12.00,
+  maxDistance: 10.00,
+  spacing: 17.00
+  })
